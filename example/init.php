45b6385112e58716c054ce9b47c855afd832e0f7
Roman Gelembjuk <roman@gelembjuk.com>
2015-07-27 07:32:40 +0300

    hide sensetive from settings

---
+++ example/init.php
@@ -0,0 +1,38 @@
+<?php 
+
+/**
+ * Example. Usage of Gelembjuk/Auth/SocialLogin library to login to a web site with social networks
+ * 
+ * This example is part of gelembjuk/auth package by Roman Gelembjuk (@gelembjuk)
+ */
+
+// path to your composer autoloader
+require ('vendor/autoload.php');
+
+$integrations = array(
+	'facebook' => array(
+		'api_key' => 'fake facebook api key',
+		'secret_key' => 'fake facebook secret key'
+		),
+	'twitter' => array(
+		'consumer_key' => 'fake twitter consumer key',
+		'consumer_secret' => 'fake twitter consumer secret'
+		),
+	'linkedin' => array(
+		'api_key' => 'fake linkedin api key',
+		'api_secret' => 'fake linkedin api secret'
+		),
+	'google' => array(
+		'application_name' => 'Your application name',
+		'client_id' => 'fake google api client id',
+		'client_secret' => 'fake google api client secret'
+		)
+	);
+
+if (file_exists('init.real.php')) {
+	// this is small trick to hide real working credentials from Git
+	// on practice you will not need this
+	include('init.real.php');
+}
+
+session_start();
